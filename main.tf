6643ea853698e60381098bf20a82929ea46ef2bf
shaik rehan <162698859+rehanReku@users.noreply.github.com>
2026-06-08 02:46:19 +0530

    Add Terraform configuration for AWS EC2 instance

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_instance" "name" {
+    count         = 1
+    ami           = "ami-091138d0f0d41ff90"
+    instance_type = "t3.micro"
+    tags         = {
+        Name = "my-ec2-instance"
+    }
+  
+}
